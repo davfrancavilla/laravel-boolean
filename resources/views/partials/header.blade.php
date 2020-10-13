2583c50a995d4ddca35a844ec16b1752e33c0572
Davide Francavilla <davfrancavilla@gmail.com>
2020-10-13 16:20:21 +0200

    Aggiunta la visualizzazione del singolo studente tramite slug

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,9 +3,9 @@
     <div class="left"><img src="https://www.boolean.careers/images/misc/logo.png" alt="logo-boolean"></div>
     <div class="right">
       <ul>
-        <li><a class="btn" href="{{route('StaticPage.home')}}">Home</a></li>
-        <li><a class="btn" href="{{route('StaticPage.chisiamo')}}">Chi siamo</a></li>
-        <li><a class="btn" href="{{route('Student.studenti')}}">Studenti</a></li>
+        <li class="{{ url()->current() == route('StaticPage.home') ? 'active' : '' }}"><a class="btn" href="{{route('StaticPage.home')}}">Home</a></li>
+        <li class="{{ url()->current() == route('StaticPage.chisiamo') ? 'active' : '' }}"><a class="btn" href="{{route('StaticPage.chisiamo')}}">Chi siamo</a></li>
+        <li class="{{ url()->current() == route('Student.studenti') ? 'active' : '' }}"><a class="btn" href="{{route('Student.studenti')}}">Studenti</a></li>
       </ul>
     </div>
   </div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,17 +4,15 @@
 @section('content')
     <section class="my-container students">
         <div class="my-cards">
-            @foreach ($data as $student)
             <div class="student-box">
                 <div class="student">
                     <img src="{{$student['img']}}" class="card-img-top" alt="{{$student['nome']}}">
-                    <a class="name" href="{{ route('Student.slug', ['slug'=>$student['slug']])}}"><h5 class="name">{{$student['nome']}}</h5></a>
-                    <h6>Assunt{{ ($student['genere'] == 'f' ? 'a' : 'o')}} da {{$student['azienda']}}</h6>
+                    <h5 class="name">{{$student['nome']}}</h5>
+                    <h6>Assunt{{ ($student['genere'] == 'f' ? 'a' : 'o')}}</h6>
                     <p class=""><small class="">{{$student['anni']}} anni</small></p>
                     <p class="">{{$student['descrizione']}}</p>
                 </div>
             </div>
-            @endforeach
         </div>
     </section>
 @endsection
