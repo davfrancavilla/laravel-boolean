9699d39df20b0bc125cab4da3740449cb38c2970
Davide Francavilla <davfrancavilla@gmail.com>
2020-10-13 09:16:40 +0200

    Modificati pulsanti

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,9 +3,9 @@
     <div class="left"><img src="https://www.boolean.careers/images/misc/logo.png" alt="logo-boolean"></div>
     <div class="right">
       <ul>
-        <li class="btn"><a href="{{route('StaticPage.home')}}">Home</a></li>
-        <li class="btn"><a href="{{route('StaticPage.chisiamo')}}">Chi siamo</a></li>
-        <li class="btn"><a href="{{route('Student.studenti')}}">Studenti</a></li>
+        <li><a class="btn" href="{{route('StaticPage.home')}}">Home</a></li>
+        <li><a class="btn" href="{{route('StaticPage.chisiamo')}}">Chi siamo</a></li>
+        <li><a class="btn" href="{{route('Student.studenti')}}">Studenti</a></li>
       </ul>
     </div>
   </div>
